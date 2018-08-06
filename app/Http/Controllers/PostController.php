b8847bfe83d9ff366e3fd5d4a1ef3b82cce0633f
gongyuhua <yg348@njit.edu>
2018-08-06 12:49:41 -0400

    Update the post controller and display the post.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Post;
 
 use Illuminate\Http\Request;
 
@@ -19,6 +20,25 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        // store code
+        $post =  new Post;
+        $post->title = $request->get('title');
+        $post->body = $request->get('body');
+
+        $post->save();
+
+        return redirect('posts');
+
+    }
+    public function index()
+    {
+        $posts = Post::all();
+
+        return view('index', compact('posts'));
+    }
+    public function show($id)
+    {
+        $post = Post::find($id);
+
+        return view('show', compact('post'));
     }
 }
